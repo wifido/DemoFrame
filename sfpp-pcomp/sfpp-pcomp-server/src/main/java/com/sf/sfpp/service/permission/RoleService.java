c104052ea86671353086ecd39921580551b15fe4
873981 <wulingjie@sf-express.com>
2016-08-10 20:42:36 +0800

    权限功能开发

---
+++ sfpp-pcomp/sfpp-pcomp-server/src/main/java/com/sf/sfpp/service/permission/RoleService.java
@@ -0,0 +1,31 @@
+/*
+* Copyright 2015-2020 SF-Express Tech Company. 
+*/
+
+package com.sf.sfpp.service.permission;
+
+import java.util.List;
+
+import com.sf.sfpp.dao.domain.permission.Role;
+import com.sf.sfpp.dao.dto.permission.UserRole;
+
+/**
+ * 
+ * @date     2016年7月27日
+ * @author   591791
+ */
+public interface RoleService {
+    
+    /** 通过工号获取用户信息*/
+    Role getRoleByRoleId(String userNo);
+	
+	List<Role> getAllRole();
+	
+	int addRole(Role role);
+	
+	int updateRole(Role role);
+	
+	List<UserRole> getUserRoleList(String userNo);
+
+    int changeUserRole(String roleId, Boolean state, String userNo);
+}
