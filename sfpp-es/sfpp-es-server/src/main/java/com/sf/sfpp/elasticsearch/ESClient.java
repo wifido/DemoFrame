e4ce1c41a65e83b545f766ca4ae3614efdc1ec41
HashZhang <zhxhash@vip.sina.com>
2016-08-17 19:56:07 +0800

    ES模块，消费写入ES

---
+++ sfpp-es/sfpp-es-server/src/main/java/com/sf/sfpp/elasticsearch/ESClient.java
@@ -0,0 +1,97 @@
+package com.sf.sfpp.elasticsearch;
+
+import com.alibaba.fastjson.JSON;
+import com.sf.sfpp.common.Constants;
+import com.sf.sfpp.pcomp.common.PcompConstants;
+import com.sf.sfpp.pcomp.common.model.PcompSoftware;
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse;
+import org.elasticsearch.action.delete.*;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.loader.PropertiesSettingsLoader;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Hash Zhang
+ * @version 1.0.0
+ * @date 2016/8/17
+ */
+public class ESClient {
+    private final Client client;
+    private final static Logger log = LoggerFactory.getLogger(ESClient.class);
+
+    public ESClient(String clusterName, String hosts) throws UnknownHostException {
+        Settings settings = Settings.settingsBuilder()
+                .put("client.transport.sniff", true)
+                .put("cluster.name", clusterName)
+                .build();
+        String host[] = hosts.split(",");
+        TransportClient build = TransportClient.builder().settings(settings).build();
+        for (int i = 0; i < host.length; i++) {
+            build.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host[i].substring(0, host[i].indexOf(":"))), Integer.parseInt(host[i].substring(host[i].indexOf(":") + 1))));
+        }
+        client = build;
+        GetResponse response = client.prepareGet("sf", "employee", "1")
+                .setOperationThreaded(false)
+                .get();
+        System.out.println(response.getSource());
+    }
+
+    public void addOrUpdateDocument(String index, String type, String id, Object document) {
+        byte[] json = JSON.toJSONBytes(document);
+        IndexResponse indexResponse = client.prepareIndex(index, type, id).setSource(json).get();
+    }
+
+    public void deleteDocument(String index, String type, String id) {
+        DeleteResponse deleteResponse = client.prepareDelete(index, type, id).get();
+    }
+
+    public SearchHit[] searchDocument(String[] indexes, String[] types, QueryBuilder queryBuilder) {
+        SearchResponse searchResponse = client.prepareSearch(indexes).setTypes(types).setQuery(queryBuilder).execute().actionGet();
+        return searchResponse.getHits().getHits();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ESClient esClient = new ESClient("hash-es", "10.202.7.184:9300,10.202.7.185:9300,10.202.7.186:9300");
+        GetMappingsResponse getMappingsResponse = esClient.client.admin().indices().prepareGetMappings(Constants.PUBLIC_COMPONENT_SYSTEM).get();
+        HashMap properties = (HashMap) getMappingsResponse.getMappings().get(Constants.PUBLIC_COMPONENT_SYSTEM).get(PcompConstants.PCOMP_SOFTWARE).getSourceAsMap().get("properties");
+        List<String> keyLists = new LinkedList<>();
+        for(Object object:properties.keySet()){
+            if(((HashMap)properties.get(object)).get("type").equals("string")){
+                keyLists.add((String) object);
+            }
+        }
+        MultiMatchQueryBuilder termQueryBuilder = QueryBuilders.multiMatchQuery("Java", keyLists.toArray(new String[]{}));
+        String []index = {Constants.PUBLIC_COMPONENT_SYSTEM};
+        String []types = {PcompConstants.PCOMP_SOFTWARE};
+        SearchHit[] searchHits = esClient.searchDocument(index, types, termQueryBuilder);
+        System.out.println(searchHits.length);
+        for (int i = 0; i < searchHits.length ; i++) {
+            System.out.println(searchHits[i].getScore());
+            System.out.println(searchHits[i].getSourceAsString());
+        }
+//        types[0] = PcompConstants.PCOMP_VERSION;
+//        searchHits = esClient.searchDocument(index, types, termQueryBuilder);
+//        System.out.println(searchHits.length);
+//        for (int i = 0; i < searchHits.length ; i++) {
+//            System.out.println(searchHits[i].getScore());
+//            System.out.println(searchHits[i].getSourceAsString());
+//        }
+    }
+}
---
+++ sfpp-es/sfpp-es-server/src/main/java/com/sf/sfpp/elasticsearch/Start.java
@@ -0,0 +1,14 @@
+package com.sf.sfpp.elasticsearch;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * @author Hash Zhang
+ * @version 1.0.0
+ * @date 2016/8/17
+ */
+public class Start {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
