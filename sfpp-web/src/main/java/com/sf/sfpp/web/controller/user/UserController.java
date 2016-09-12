b472ed997fbbf70f58e890d0baef67519d2515d1
01139940 <zhangyutao3@sf-express.com>
2016-09-12 21:07:14 +0800

    处理冲突

--- sfpp-web/src/main/java/com/sf/sfpp/web/controller/user/UserController.java
+++ sfpp-web/src/main/java/com/sf/sfpp/web/controller/user/UserController.java
@@ -1,7 +1,13 @@
 package com.sf.sfpp.web.controller.user;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.github.pagehelper.PageInfo;
+import com.sf.sfpp.common.dto.JsonResult;
+import com.sf.sfpp.common.utils.ExceptionUtils;
+import com.sf.sfpp.user.dao.domain.User;
+import com.sf.sfpp.user.dao.domain.UserHistory;
+import com.sf.sfpp.user.service.UserService;
+import com.sf.sfpp.user.service.UserHistoryService;
+import com.sf.sfpp.web.controller.common.AbstractCachedController;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
@@ -12,13 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.github.pagehelper.PageInfo;
-import com.sf.sfpp.common.dto.JsonResult;
-import com.sf.sfpp.common.utils.ExceptionUtils;
-import com.sf.sfpp.user.dao.domain.User;
-import com.sf.sfpp.user.dao.domain.UserHistory;
-import com.sf.sfpp.user.service.UserHistoryService;
-import com.sf.sfpp.user.service.UserService;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Hash Zhang
@@ -26,9 +26,11 @@ import com.sf.sfpp.user.service.UserService;
  * @date 2016/9/2
  */
 @Controller
-public class UserController {
+public class UserController extends AbstractCachedController {
     @Autowired
     private UserService userService;
+
+    @Autowired
     private UserHistoryService userHistoryService;
 
     private final static Logger log = LoggerFactory.getLogger(UserController.class);
@@ -36,10 +38,11 @@ public class UserController {
     /**
      * 去除隐私信息（密码等），只保留给前端展示的
      * todo: 如果用户结构有修改，那么一定要修改这里
+     *
      * @param source
      * @return
      */
-    private User getPureUserInfo(User source){
+    private User getPureUserInfo(User source) {
         User user = new User();
         user.setId(source.getId());
         user.setCreatedTime(source.getCreatedTime());
@@ -57,13 +60,14 @@ public class UserController {
             Subject currentUser = SecurityUtils.getSubject();
             User user = getPureUserInfo((User) currentUser.getPrincipal());
             userJsonResult.setData(user);
-        }catch(Exception e){
+        } catch (Exception e) {
             String stackTrace = ExceptionUtils.getStackTrace(e);
             log.warn(stackTrace);
             userJsonResult.setMessage(stackTrace);
         }
         return userJsonResult;
     }
+
     @ResponseBody
     @RequestMapping(value = "user/getUserInfo", method = RequestMethod.GET)
     public JsonResult<User> getUserInfo(HttpServletRequest request) {
@@ -72,16 +76,17 @@ public class UserController {
             int userId = Integer.parseInt(request.getParameter("userId"));
             User user = getPureUserInfo(userService.getUserByUserId(userId));
             userJsonResult.setData(user);
-        }catch(Exception e){
+        } catch (Exception e) {
             String stackTrace = ExceptionUtils.getStackTrace(e);
             log.warn(stackTrace);
             userJsonResult.setMessage(stackTrace);
         }
         return userJsonResult;
     }
-    
+
+    @ResponseBody
     @RequestMapping(value = "user/getHistory", method = RequestMethod.GET)
-    public JsonResult<PageInfo<UserHistory>> getUserHistoryPage(HttpServletRequest request) {
+    public JsonResult<PageInfo<UserHistory>> userHistoryPage(HttpServletRequest request) {
         JsonResult<PageInfo<UserHistory>> userHistoryJsonResult = new JsonResult<>();
         try {
             int userId = Integer.parseInt(request.getParameter("userId"));
@@ -95,4 +100,5 @@ public class UserController {
         }
         return userHistoryJsonResult;
     }
+
 }
